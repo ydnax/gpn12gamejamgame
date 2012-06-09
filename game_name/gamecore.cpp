4f928fecd6978ce94a805ae802815c67467b18b5
xandy <gitmail.xandy@xoxy.net>
2012-06-09 04:28:50 +0200

    improved level generation

--- game_name/gamecore.cpp
+++ game_name/gamecore.cpp
@@ -4,27 +4,31 @@
 #include <game_name/Objects/Unit.hpp>
 #include <game_name/Objects/Node.hpp>
 #include <game_name/GameLogic/UnitCtrl.hpp>
+#include <iostream>
 namespace picppgl{
 gamecore::gamecore(){
     window=new mainwindow(800, 500);
+    auto lvl1=[]()->Level*{
+        auto lvl=new Level{};
+        new Node(50,50,lvl);
+        new Node(200,100,lvl);
+        new Node(100,200,lvl);
+        return lvl;
+    };
+    levels.push_back(lvl1);
 }
+    
 void gamecore::gameloop(){
     timer.start();
-    auto lvl=new Level{};
-    /*
-    new Unit(1, 1, 100, 100, lvl);
-    new Unit(100, 100, 1, 1, lvl);
-    new Unit(100, 100, 300, 10, lvl);
-    new Unit(300, 10, 100, 100, lvl);//*/
-    new Node(50,50,lvl);
-    new Node(200,100,lvl);//*/
-    new Node(100,200,lvl);
-
-    while(true){
-        window->render();
-        lvl->update(timer.get_dticks());
-        clickctrl->handleInput();
+    for(auto &&lgen:levels){
+        auto level=lgen();
+        while(true){
+            window->render();
+            level->update(timer.get_dticks());
+            clickctrl->handleInput();
+        }
     }
+    std::cout<<"all levels played. game over"<<std::endl;
 }
 
 }
--- game_name/gamecore.hpp
+++ game_name/gamecore.hpp
@@ -1,12 +1,17 @@
 #ifndef gamejam_gamecore_hpp
 #define gamejam_gamecore_hpp
+#include <functional>
+#include <vector>
 #include <pic-gl/Util/gametimer.hpp>
 namespace picppgl{
 class mainwindow;
+class Level;
 class gamecore{
 private:
     GameTimer timer;
     mainwindow* window;
+    typedef std::function<Level*()> LvlFun;
+    std::vector<LvlFun> levels;
 public:
     gamecore();
     void gameloop();

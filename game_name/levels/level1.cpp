824ccf5b81c2528455ebcb9f55c95b0bad8bdad5
xandy <gitmail.xandy@xoxy.net>
2012-06-09 16:06:09 +0200

    updated tutorial instructions

--- game_name/levels/level1.cpp
+++ game_name/levels/level1.cpp
@@ -7,7 +7,7 @@
     auto n3 = new Node(100,400,lvl);
     n3->unitcount(4);
 
-    new ImgObj(100, 200, Image("Click a town to select it, click it again to deselect.", 12), lvl);
+    new ImgObj(100, 200, Image("Click a town to select it, click it again or press space to deselect.", 12), lvl);
     new ImgObj(150, 250, Image("After selecting one of your towns, click another one to send a robot.", 12), lvl);
     new ImgObj(200, 300, Image("To win a level if you hold all towns.", 12), lvl);
     n3->Owner(Player::Ki);
